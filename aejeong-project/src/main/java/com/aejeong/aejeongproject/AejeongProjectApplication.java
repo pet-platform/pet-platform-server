10da1314ec2c52347a96c165cf996d75d5f66375
imjunhui <yim3370@gmail.com>
2023-07-18 23:54:25 +0900

    애정 스프링 부트 프로젝트 기본 설정

---
+++ aejeong-project/src/main/java/com/aejeong/aejeongproject/AejeongProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aejeong.aejeongproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AejeongProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AejeongProjectApplication.class, args);
+	}
+
+}
