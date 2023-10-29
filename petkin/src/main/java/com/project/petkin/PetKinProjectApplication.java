da68138d6131a24409b6cd8eedf4cf2d54b62973
imjunhui <yim3370@gmail.com>
2023-10-29 21:31:12 +0900

    feat: test pr
    Application change
    feature/ijh

--- petkin/src/main/java/com/project/petkin/PetKinProjectApplication.java
+++ petkin/src/main/java/com/project/petkin/PetKinProjectApplication.java
@@ -2,8 +2,10 @@ package com.project.petkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PetKinProjectApplication {
 
 	public static void main(String[] args) {
