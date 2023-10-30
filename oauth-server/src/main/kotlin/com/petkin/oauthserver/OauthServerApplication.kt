026cb9f8fcba86900a08106a556754780bcd55d1
imjunhui <yim3370@gmail.com>
2023-10-30 21:56:14 +0900

    feat: Project Test - Discord PR Webhook test - OAuth-Server project initialize

--- oauth-server/src/main/kotlin/com/petkin/oauthserver/OauthServerApplication.kt
+++ oauth-server/src/main/kotlin/com/petkin/oauthserver/OauthServerApplication.kt
@@ -1,4 +1,4 @@
-package com.example.oauthserver
+package com.petkin.oauthserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
