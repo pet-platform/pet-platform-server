8eff76e88bf8ae0346c884e4455345de29546db0
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2023-11-26 19:03:59 +0900

    feat : Email API 작업 - Email API(EmailController, EmailService, RandomCodeGenerator, VerificationRequest)

---
+++ MemberService/src/main/java/service/random/RandomCodeGenerator.java
@@ -0,0 +1,19 @@
+package service.random;
+
+import java.security.SecureRandom;
+
+public class RandomCodeGenerator {
+    private static final String CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final int CODE_LENGTH = 5;
+
+    public static String generateRandomCode() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder code = new StringBuilder(CODE_LENGTH);
+
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            code.append(CHARACTERS.charAt(randomIndex));
+        }
+        return code.toString();
+    }
+}
