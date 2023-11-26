b8da3d8c4a45a707ad6ff0779518fbb466ab2d3b
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2023-11-26 19:03:59 +0900

    feat : Email API 작업 - Email API(EmailController, EmailService, RandomCodeGenerator, VerificationRequest)

--- MemberService/src/main/java/service/EmailService.java
+++ MemberService/src/main/java/service/EmailService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import service.random.RandomCodeGenerator;
 
 @Service
 public class EmailService {
@@ -11,11 +12,13 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void sendEmail(String to, String subject, String body) {
+    public void sendVerificationCode(String to) {
+
+        String verificationCode = RandomCodeGenerator.generateRandomCode();
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
+        message.setSubject("이메일 인증 코");
+        message.setText("인증 코드: " + verificationCode);
 
         javaMailSender.send(message);
     }
