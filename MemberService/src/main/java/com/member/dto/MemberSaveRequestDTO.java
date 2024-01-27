0e7b4a97b6a47cd91ccde6d8b1feacbfd01f6cfe
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2024-01-27 21:46:16 +0900

    feat : 일반 회원가입 API(Validator)

---
+++ MemberService/src/main/java/com/member/dto/MemberSaveRequestDTO.java
@@ -0,0 +1,23 @@
+package com.member.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberSaveRequestDTO {
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    private String password;
+
+    @NotBlank(message = "닉네임을 입력해주세요")
+    private String nickname;
+}
