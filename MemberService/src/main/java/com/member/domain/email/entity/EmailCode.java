58a85873a0b48359e750153b26a5548ba444bc75
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2024-01-29 17:58:18 +0900

    feat : Email API(EmailCode, EmailCodeService, EmailService, JoinEmailCodeRepository)

---
+++ MemberService/src/main/java/com/member/domain/email/entity/EmailCode.java
@@ -0,0 +1,25 @@
+package com.member.domain.email.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class EmailCode {
+
+    @Id
+    private String name;
+
+    private String email;
+    private String code;
+
+    @Builder
+    public EmailCode(String name, String code, String email) {
+        this.name = name;
+        this.code = code;
+        this.email = email;
+    }
+}
