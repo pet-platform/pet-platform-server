235921721671e3bbfd5f772031babbd22ae48152
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2024-02-04 17:26:43 +0900

    feat : 회원가입 API(password validator 추가)

--- MemberService/src/main/java/com/member/domain/member/service/MemberAuthService.java
+++ MemberService/src/main/java/com/member/domain/member/service/MemberAuthService.java
@@ -3,6 +3,7 @@ package com.member.domain.member.service;
 import com.member.domain.member.dto.MemberSaveRequestDTO;
 import com.member.domain.member.entity.Member;
 import com.member.domain.member.repository.MemberRepository;
+import com.member.global.validator.PasswordValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,9 +30,15 @@ public class MemberAuthService {
             return false;
         }
         final Member member = SaveRequestToMember(requestDTO);
-        final String encodePassword = bCryptPasswordEncoder.encode(member.getPassword());
-        member.setEncodedPassword(encodePassword);
-        memberRepository.save(member);
+        String password = requestDTO.getPassword();
+        if (!PasswordValidator.validate(password)) {
+            log.error("비밀번호는 최소 8자 이상이어야 하며, 최소 1개의 숫자, 1개의 소문자, 1개의 대문자, 1개의 특수 문자를 포함해야 합니다.");
+            return false;
+        } else {
+            String encodePassword = bCryptPasswordEncoder.encode(password);
+            member.setEncodedPassword(encodePassword);
+            memberRepository.save(member);
+        }
         return true;
     }
 
