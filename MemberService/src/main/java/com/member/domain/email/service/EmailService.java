cb43ad147c1d48b6ba9a8f4faa729640fd57f003
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2024-02-07 21:34:07 +0900

    feat : mailService 수정

--- MemberService/src/main/java/com/member/domain/email/service/EmailService.java
+++ MemberService/src/main/java/com/member/domain/email/service/EmailService.java
@@ -3,18 +3,20 @@ package com.member.domain.email.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
-    public void sendEmail(String email, String subject, String content) {
+    public boolean sendEmail(String email, String subject, String content) {
         final MimeMessage message = javaMailSender.createMimeMessage();
         try {
             final MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
@@ -22,8 +24,10 @@ public class EmailService {
             messageHelper.setSubject(subject);
             messageHelper.setText(content, true);
             javaMailSender.send(message);
+            return true;
         } catch (MessagingException e) {
-            throw new RuntimeException(e);
+            log.error("메시지 발송에 실패했습니다");
         }
+        return false;
     }
 }
