36d104bc88dc3880cecb39bd87d836cb5eeeceaf
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2024-02-07 21:34:42 +0900

    feat : config

--- MemberService/src/main/java/com/member/domain/terms/entity/TermsAgreement.java
+++ MemberService/src/main/java/com/member/domain/terms/entity/TermsAgreement.java
@@ -8,14 +8,16 @@ import java.time.LocalDateTime;
 @Table(name = "TERMS_AGREEMENT")
 @Entity
 public class TermsAgreement {
+    @Id
+    @OneToOne
+    @JoinColumn(name = "TERMS_CODE")
+    private TermsDetails termsDetails;
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
     @Column(name = "TERMS_AGREEMENT_FLAG")
     private boolean agreementFlag;
     @Column(name = "TERMS_AGREEMENT_DATE")
     private LocalDateTime agreementDate;
-    @ManyToOne
-    @JoinColumn(name = "MEMBER_ID")
-    private Member member;
-    @OneToOne
-    @JoinColumn(name = "TERMS_CODE")
-    private TermsDetails termsDetails;
 }
